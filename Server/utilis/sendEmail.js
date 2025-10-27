cfcfb9cdaa9a78197e7ca7af4fed2d078f2686aa
ElakshmanaRaj <elakshmanabca07@gmail.com>
2025-10-27 17:00:20 +0530

    fix mail issues

--- Server/utilis/sendEmail.js
+++ Server/utilis/sendEmail.js
@@ -2,9 +2,6 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
-  host: "smtp.ethereal.email",
-  port: 587,
-  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS, 
