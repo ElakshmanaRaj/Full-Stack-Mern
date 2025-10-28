8b4b4fdef5ce135b76172b38b84e2250906e8652
ElakshmanaRaj <elakshmanabca07@gmail.com>
2025-10-28 09:10:06 +0530

    fix

--- Server/controllers/productController.js
+++ Server/controllers/productController.js
@@ -1,41 +1,53 @@
 
 const Product = require("../models/Product");
+const cloudinary = require("cloudinary").v2;
 
 const createProduct = async (req, res) => {
-    try {
-      const { name, description, category, price, stock } = req.body;
-  
-      if (!name || !description || !category || !price || !stock) {
-        return res.status(400).json({ message: "Please fill all fields" });
-      }
-  
-      let imageUrls = [];
-      if (req.files && req.files.length > 0) {
-        imageUrls = req.files.map((file) =>
-          `${req.protocol}://${req.get("host")}/products/${file.filename}`
-        );
-      }
-  
-      const product = await Product.create({
-        name,
-        description,
-        category,
-        price,
-        stock,
-        images: imageUrls,
-      });
-  
-      res.status(201).json({
-        success: true,
-        message: "Product created successfully",
-        product,
-      });
-    } catch (err) {
-      console.error("Error creating product:", err);
-      res.status(500).json({ success: false, error: err.message });
+
+  try {
+
+    const { name, description, category, price, stock } = req.body;
+
+    if (!name || !description || !category || !price || !stock) {
+      return res.status(400).json({ message: "Please fill all fields" });
     }
-  };
-  
+
+    let imageUrls = [];
+
+    if (req.files && req.files.length > 0) {
+      const uploadPromises = req.files.map(async (file) => {
+        const result = await cloudinary.uploader.upload(file.path, {
+          folder: "products", 
+        });
+        return result.secure_url;
+      });
+
+      imageUrls = await Promise.all(uploadPromises);
+    }
+
+   
+    const product = await Product.create({
+      name,
+      description,
+      category,
+      price,
+      stock,
+      images: imageUrls,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Product created successfully",
+      product,
+    });
+  } catch (err) {
+    console.error("Error creating product:", err);
+    res.status(500).json({ success: false, error: err.message });
+  }
+};
+
+
+
 
 const getProducts = async (req, res) => {
     try {
