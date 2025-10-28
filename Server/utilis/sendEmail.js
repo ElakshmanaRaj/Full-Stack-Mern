ed971f906a94d7c7d9c92a254f17e2f934a23057
ElakshmanaRaj <elakshmanabca07@gmail.com>
2025-10-28 11:43:36 +0530

    fix resend issue

--- Server/utilis/sendEmail.js
+++ Server/utilis/sendEmail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendMail = async (to, subject, text) => {
   try {
     await resend.emails.send({
-      from: '<onboarding@resend.dev>',
+      from: 'ShopNest <onboarding@resend.dev>',
       to,
       subject,
       text,
