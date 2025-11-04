503e914bf08c13b06adfa2b7d43c4dd43f24699f
ElakshmanaRaj <elakshmanabca07@gmail.com>
2025-11-04 17:11:00 +0530

    added new code

--- Server/utilis/sendEmail.js
+++ Server/utilis/sendEmail.js
@@ -2,28 +2,32 @@
 const axios = require("axios");
 
 const sendMail = async (to, subject, html) => {
-
   try {
+    console.log("EMAIL_USER:", process.env.EMAIL_USER);
+    console.log("API_KEY loaded:", !!process.env.API_KEY);
 
-    const response = await axios.post("https://api.brevo.com/v3/smtp/email", {
-      sender: {
-        name: "Shopnest Ecommerce",
-        email: process.env.EMAIL_USER, 
+    const response = await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      {
+        sender: {
+          name: "Shopnest Ecommerce",
+          email: process.env.EMAIL_USER,
+        },
+        to: [{ email: to }],
+        subject,
+        htmlContent: html,
       },
-      to: [{ email: to }],
-      subject,
-      htmlContent: html,
-    },
-  {
-    headers:{
-      "api-key": process.env.API_KEY,
-      "Content-Type":"application/json",
-    }
-  });
+      {
+        headers: {
+          "api-key": process.env.API_KEY,
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-  console.log("Email sent successfully:", response.data);
+    console.log("Email sent successfully:", response.data);
   } catch (error) {
-    console.error("Error sending email:", error.message ||  error.response?.data);
+    console.error("Error sending email:", error.response?.data || error.message);
   }
 };
 
